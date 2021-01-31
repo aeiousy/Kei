3e7b4941de9460ef38581e3593694d2b09ec8cbc
aeiousy <71985983+aeiousy@users.noreply.github.com>
2021-01-30 23:38:07 -0500

    Update invite.js

--- commands/bot/invite.js
+++ commands/bot/invite.js
@@ -1,18 +1,18 @@
 const Discord = require('discord.js')
 
 module.exports = {
-    name: "ping",
+    name: "invite",
     description: "test command",
     group: "bot",
 
     async run (client, message, args) {
 
 
-        const ping = new Discord.MessageEmbed()
+        const invite = new Discord.MessageEmbed()
         .setColor("GREY")
         .setDescription("https://discord.com/api/oauth2/authorize?client_id=702794967403462656&permissions=8&scope=bot");
 
 
-        message.channel.send(ping);
+        message.channel.send(invite);
     }
 }
